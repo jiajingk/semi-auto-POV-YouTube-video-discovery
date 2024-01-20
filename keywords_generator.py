1b9d55452a7d7b11d3d7400b58c7eadf6bd5c125
Jingkai <jiajingkai1997@gmail.com>
2024-01-20 13:36:26 -0500

    bug fixing
    the keywords generator should generate nothing when total count
    is reached

--- keywords_generator.py
+++ keywords_generator.py
@@ -10,8 +10,11 @@ class KeywordsGenerator:
     def next_keyworks(self, count: int = 1) -> Iterator[List[str]]:
         total_count = 0
         for catorgory in self.keywords:
+            if total_count > count:
+                break
             for keyword_sentence in self.keywords[catorgory]:
                 if total_count > count:
                     break
                 yield keyword_sentence.split(' ')
                 total_count += 1
+            
